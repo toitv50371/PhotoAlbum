9fab471248f7d26a38f6cd2559bc4616baf616ac
Toi Tran <toitv.astary@gmail.com>
2023-08-18 16:48:59 +0700

    update

--- photoShare/settings.py
+++ photoShare/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,13 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-ke5u(my3q-_18=d$=n89ck&i=2^k)flp8152m!4x$%oyfa)od3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split("")
-
+ALLOWED_HOSTS = []
 
 
 # Application definition
