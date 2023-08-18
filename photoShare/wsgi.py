19ac208c2f98890128492424dbb1a5895c33b1de
Toi Tran <toitv.astary@gmail.com>
2023-08-18 11:17:18 +0700

    update project

--- photoShare/wsgi.py
+++ photoShare/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photoShare.settings')
 
 application = get_wsgi_application()
+
+app = application
